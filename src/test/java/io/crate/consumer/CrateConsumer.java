c8cc9e915db0d9909feb064e9de52081d9056dd8
Michael Beer <michael.beer@crate.io>
2016-05-03 10:49:56 +0200

    use http as crate consumer client

--- src/test/java/io/crate/consumer/CrateConsumer.java
+++ src/test/java/io/crate/consumer/CrateConsumer.java
@@ -2,23 +2,21 @@ package io.crate.consumer;
 
 import com.carrotsearch.junitbenchmarks.AutocloseConsumer;
 import com.carrotsearch.junitbenchmarks.Result;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import io.crate.action.sql.SQLRequest;
-import io.crate.client.CrateClient;
+import com.google.gson.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.log4j.Logger;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,19 +29,18 @@ public final class CrateConsumer extends AutocloseConsumer implements Closeable 
 
     private final String testServerHost;
     private final int testServerHttpPort;
-    private final CrateClient client;
+    private final HttpClient httpClient;
 
     public CrateConsumer(String testServerHost, int testServerHttpPort) {
+        this.httpClient = HttpClientBuilder.create().build();
         this.testServerHost = testServerHost;
         this.testServerHttpPort = testServerHttpPort;
-        client = new CrateClient(composeUrls(getHosts(), getPort(CrateConsumerConstants.CRATE_TRANSPORT_PORT)));
-        client.sql(CrateConsumerConstants.CREATE_TABLE_IF_NOT_EXISTS_STMT).actionGet();
     }
 
-    private static String[] getHosts() {
+    private static String getHost() {
         final String hostsStr = System.getProperty(CrateConsumerConstants.CRATE_HOST);
         if (hostsStr != null && !hostsStr.trim().equals("")) {
-            return hostsStr.split(",");
+            return hostsStr;
         }
         throw new IllegalArgumentException(String
                 .format("Missing global property: %s", CrateConsumerConstants.CRATE_HOST));
@@ -58,10 +55,8 @@ public final class CrateConsumer extends AutocloseConsumer implements Closeable 
                 .format("Missing global property: %s", portProperty));
     }
 
-    private static String[] composeUrls(String[] hosts, int port) {
-        return Arrays.stream(hosts)
-                .map(host -> String.format("http://%s:%d", host, port))
-                .toArray(String[]::new);
+    private static String composeURI(String host, int port) {
+        return String.format("http://%s:%d/_sql", host, port);
     }
 
     @Override
@@ -77,7 +72,16 @@ public final class CrateConsumer extends AutocloseConsumer implements Closeable 
                     System.currentTimeMillis(),
                     benchmarkValues(result)
             };
-            client.sql(new SQLRequest(CrateConsumerConstants.INSERT_STMT, args)).actionGet();
+            Gson gson = new GsonBuilder().create();
+            JsonObject content = new JsonObject();
+            content.add("args", gson.toJsonTree(args));
+            content.addProperty("stmt", CrateConsumerConstants.INSERT_STMT);
+            HttpPost request = new HttpPost(composeURI(getHost(), getPort(CrateConsumerConstants.CRATE_HTTP_PORT)));
+            request.setEntity(new StringEntity(gson.toJson(content)));
+            HttpResponse res = httpClient.execute(request);
+            if (res.getStatusLine().getStatusCode() != 200) {
+                LOGGER.error("Failed to store benchmark result");
+            }
         } catch (ParseException e) {
             LOGGER.warn("Cannot parse timestamp: ", e);
         } catch (IOException e) {
@@ -103,10 +107,9 @@ public final class CrateConsumer extends AutocloseConsumer implements Closeable 
     }
 
     private JsonObject clusterVersionInfo() throws IOException {
-        URL url = new URL(String.format("http://%s:%d", testServerHost, testServerHttpPort));
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.connect();
-        JsonElement root = jsonParser.parse(new InputStreamReader((InputStream) connection.getContent()));
+        HttpGet request = new HttpGet(composeURI(testServerHost, testServerHttpPort));
+        HttpResponse response = httpClient.execute(request);
+        JsonElement root = jsonParser.parse(new InputStreamReader(response.getEntity().getContent()));
         return root.getAsJsonObject()
                 .get("version").getAsJsonObject();
     }
@@ -126,9 +129,6 @@ public final class CrateConsumer extends AutocloseConsumer implements Closeable 
 
     @Override
     public void close() throws IOException {
-        if (client != null) {
-            client.close();
-        }
     }
 
 }
