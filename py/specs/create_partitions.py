697eaa9684d7fa5a73939d907fb8b7b231f313db
kovrus <ruslan@crate.io>
2016-08-29 13:53:25 +0300

    add iterations to the create partitions benchmark

--- py/specs/create_partitions.py
+++ py/specs/create_partitions.py
@@ -1,11 +1,23 @@
 create_table_stmt = """
-create table t (
-    x int,
-    p int
-)
-clustered into 1 shards
-partitioned by (p)
-with (number_of_replicas = 0)"""
+    create table t (
+        x int,
+        p int
+    )
+    clustered into 1 shards
+    partitioned by (p)
+    with (number_of_replicas=0, refresh_interval=0)"""
+
+class BulkArgsGenerator:
+
+    def __init__(self, bulk_size):
+        self.count = 0
+        self.bulk_size = bulk_size
+
+    def __call__(self):
+        start = self.count * self.bulk_size
+        end = start + self.bulk_size
+        self.count += 1
+        return [[x, x] for x in range(start, end)]
 
 spec = Spec(
     setup=Instructions(statements=[create_table_stmt]),
@@ -13,7 +25,8 @@ spec = Spec(
     queries=[
         {
             'statement': 'insert into t (x, p) values (?, ?)',
-            'bulk_args': [[i, i] for i in range(500)]
+            'bulk_args': BulkArgsGenerator(20),
+            'iterations': 30,
         }
     ]
 )
