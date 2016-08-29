0f9cfff52e0ae92184d2603f97f4a88a0e0c0102
kovrus <ruslan@crate.io>
2016-08-29 11:43:06 +0300

    add the information schema benchmark

---
+++ py/specs/information_schema.py
@@ -0,0 +1,16 @@
+create_table_stmt = """create table table_{} (name string)
+                       clustered into 1 shards
+                       with (number_of_replicas=0)"""
+
+drop_table_stmt = "drop table table_{}"
+
+spec = Spec(
+    setup=Instructions(statements=[create_table_stmt.format(i) for i in range(150)]),
+    teardown=Instructions(statements=[drop_table_stmt.format(i) for i in range(150)]),
+    queries=[
+        {
+            'statement': "select * from information_schema.tables",
+            'iterations': 100000,
+        }
+    ]
+)
