c2cb29139dd222a25261e54369300e413caeb7b6
Christian Haudum <christian.haudum@gmail.com>
2016-09-12 13:48:15 +0200

    disable refresh during copy from

--- py/specs/sql/uservisits.sql
+++ py/specs/sql/uservisits.sql
@@ -1,11 +1,14 @@
 create table uservisits (
-    "sourceIP" string primary key,
-    "destinationURL" string,
-    "visitDate" timestamp,
-    "adRevenue" float,
-    "UserAgent" string INDEX using fulltext,
-    "cCode" string,
-    "lCode" string,
-    "searchWord" string,
-    "duration" int
-) with (number_of_replicas = 0);
+   "sourceIP" string primary key,
+   "destinationURL" string,
+   "visitDate" timestamp,
+   "adRevenue" float,
+   "UserAgent" string INDEX using fulltext,
+   "cCode" string,
+   "lCode" string,
+   "searchWord" string,
+   "duration" int
+) with (
+    number_of_replicas = 0,
+    refresh_interval = 0,
+);
