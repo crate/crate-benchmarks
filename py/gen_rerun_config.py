4bc92212b22bed7ca0195692a39641bab44eb84e
Christian Haudum <christian.haudum@gmail.com>
2016-09-19 16:20:29 +0200

    made it possible to also generate rerun config for versions

--- py/gen_rerun_config.py
+++ py/gen_rerun_config.py
@@ -16,37 +16,27 @@ def parse_args():
     Parse command line arguments
     """
     parser = argparse.ArgumentParser(description='')
+    subparsers = parser.add_subparsers()
     parser.add_argument('--infile', '-i',
                         type=argparse.FileType('r'), default=sys.stdin,
                         help='base configuration file')
     parser.add_argument('--outfile', '-o',
                         type=argparse.FileType('w'), default=sys.stdout,
                         help='base configuration file')
-    parser.add_argument('--num-releases', '-n',
-                        type=int, default=30,
-                        help='number of releases')
+    nightlies = subparsers.add_parser('nightlies')
+    nightlies.add_argument('--num-releases', '-n',
+                           type=int, required=True,
+                           metavar='NUM',
+                           help='number of releases')
+    nightlies.set_defaults(func=gen_nightlies)
+    versions = subparsers.add_parser('versions')
+    versions.add_argument('--versions', '-v',
+                          type=str, nargs='+', required=True,
+                          metavar='VERSION',
+                          help='list of version numbers')
+    versions.set_defaults(func=gen_versions)
     return parser.parse_args()
 
-
-def _extract_nightly_uri(base_uri, line):
-    """
-    Extract full URL from line
-    """
-    m = NIGHTLY_RE.match(line)
-    if m:
-        return base_uri + m.group('filename')
-
-
-def fetch(n):
-    """
-    Load URLs for nightly builds
-    Returns a list of n nightly build URLs
-    """
-    base_uri = 'https://cdn.crate.io/downloads/releases/nightly/'
-    versions = []
-    with request.urlopen(base_uri) as r:
-        versions = [_extract_nightly_uri(base_uri, line.decode('utf-8')) for line in r]
-    return [v for v in reversed(versions) if v][:n]
 
 def _read(fp):
     """
@@ -62,19 +52,47 @@ def _write(fp, data):
     return fp.write(toml.dumps(data))
 
 
+def _extract_nightly_uri(base_uri, line):
+    """
+    Extract full URL from line
+    """
+    m = NIGHTLY_RE.match(line)
+    if m:
+        return base_uri + m.group('filename')
+
+
+def gen_nightlies(ns):
+    """
+    Load URLs for nightly builds
+    Returns a list of n nightly build URLs
+    """
+    base_uri = 'https://cdn.crate.io/downloads/releases/nightly/'
+    versions = []
+    with request.urlopen(base_uri) as r:
+        versions = [_extract_nightly_uri(base_uri, line.decode('utf-8')) for line in r]
+    return [v for v in reversed(versions) if v][:ns.num_releases]
+
+
+def gen_versions(ns):
+    """
+    Returns the list provided by the CLI argument
+    """
+    return ns.versions
+
+
 def main():
     ns = parse_args()
     conf = _read(ns.infile)
-    conf['versions'] = fetch(ns.num_releases)
+    conf['versions'] = ns.func(ns)
     _write(ns.outfile, conf)
 
 
 if __name__ == '__main__':
     """
     Example usage:
-    python gen_rerun_conf.py < tracks/latest.toml
+    python gen_rerun_conf.py < tracks/latest.toml versions 0.55.4 0.55.5
     or:
-    python gen_rerun_conf.py -i tracks/latest.toml -o tracks/rerun.toml -n 10
+    python gen_rerun_conf.py -i tracks/latest.toml -o tracks/rerun.toml nightlies -n 10
 
     For help run:
     python gen_rerun_conf.py --help
