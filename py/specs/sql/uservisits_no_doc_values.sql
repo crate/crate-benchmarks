bc5378872487e2ef30e205f7cfddd7e504344b4d
Sebastian Utz <su@rtme.net>
2017-12-01 13:28:55 +0100

    adds benchmarks for disabled doc_values/column store
    groupings, aggregates and ordering on columns with disabled doc_values
    uses a source lookup. this benchmarks are intended to be compared to the
    equivalent benchmarks with enabled doc_values to see the performance
    differences

---
+++ py/specs/sql/uservisits_no_doc_values.sql
@@ -0,0 +1,16 @@
+CREATE TABLE uservisits_no_doc_values (
+   "sourceIP" STRING PRIMARY KEY,
+   "destinationURL" STRING,
+   "visitDate" TIMESTAMP,
+   "adRevenue" FLOAT STORAGE WITH (columnstore = false),
+   "UserAgent" STRING INDEX USING FULLTEXT,
+   "cCode" STRING STORAGE WITH (columnstore = false),
+   "lCode" STRING,
+   "searchWord" STRING,
+   "duration" INTEGER,
+   INDEX uagent_plain USING PLAIN("UserAgent")
+   -- ^^ used for regex matching ^^ --
+) WITH (
+    number_of_replicas = 0,
+    refresh_interval = 0
+);
