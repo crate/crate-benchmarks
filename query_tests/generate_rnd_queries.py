2abae7b42e226efc6d920da3eb8154b4b2346c76
Andrei Dan <andrei@crate.io>
2018-05-17 07:55:13 +0100

    Randomly use scalar functions in random query tests.

--- query_tests/generate_rnd_queries.py
+++ query_tests/generate_rnd_queries.py
@@ -20,6 +20,56 @@ from cr8.bench_spec import Spec, Instructions
 from crate.client import connect
 
 
+def generate_one_param_function_clause(function_name,
+                                       column,
+                                       provider,
+                                       abs_column=False):
+    compare_to = provider()
+    if abs_column is True:
+        return f"{function_name}(abs({column})) != {compare_to}"
+    else:
+        return f"{function_name}({column}) != {compare_to}"
+
+
+def generate_arc_function_clause(function_name, column, provider):
+    """ Arc sin/cos/tan functions need the input to be in the [-1.0,1.0]
+    interval. This generates a function clause that makes sure the arc
+    functions are not called with invalid input.
+    """
+
+    compare_to = provider()
+    return f"{function_name}({column}/({column} + random())) != {compare_to}"
+
+
+def generate_two_param_function_clause(function_name,
+                                       column,
+                                       provider,
+                                       abs_column=False):
+    second_arg = provider()
+    compare_to = provider()
+    if abs_column is True:
+        return f"{function_name}(abs({column}), {second_arg}) != {compare_to}"
+    else:
+        return f"{function_name}({column}, {second_arg}) != {compare_to}"
+
+
+# Array functions
+
+
+def array_cat(data_faker, column, provider):
+    generate_two_param_function_clause('ARRAY_CAT', column, provider)
+
+
+def array_unique(data_faker, column, provider):
+    generate_two_param_function_clause('ARRAY_UNIQUE', column, provider)
+
+
+def array_difference(data_faker, column, provider):
+    generate_two_param_function_clause('ARRAY_DIFFERENCE', column, provider)
+
+# Geo functions
+
+
 def within(data_faker, column, provider):
     arg2 = provider()
     return f"WITHIN({column}, '{arg2}')"
@@ -31,8 +81,149 @@ def distance(data_faker, column, provider):
     return f"DISTANCE({column}, {other_point}) < {dist}"
 
 
+def intersects(data_faker, column, provider):
+    other_shape = provider()
+    return f"INTERSECTS({column}, {other_shape})"
+
+
+def latitude(data_faker, column, provider):
+    gen_lat = data_faker.fake.random_int(min=1, max=90)
+    return f"LATITUDE({column}) < {gen_lat}"
+
+
+def longitude(data_faker, column, provider):
+    gen_long = data_faker.fake.random_int(min=1, max=180)
+    return f"LONGITUDE({column}) < {gen_long}"
+
+
+def geohash(data_faker, column, provider):
+    return generate_one_param_function_clause('GEOHASH', column, provider)
+
+# Numbers functions
+
+
+def abs(data_faker, column, provider):
+    return generate_one_param_function_clause('ABS', column, provider)
+
+
+def ceil(data_faker, column, provider):
+    return generate_one_param_function_clause('CEIL', column, provider)
+
+
+def floor(data_faker, column, provider):
+    return generate_one_param_function_clause('FLOOR', column, provider)
+
+
+def ln(data_faker, column, provider):
+    return generate_one_param_function_clause('LN', column, provider, True)
+
+
+def log(data_faker, column, provider):
+    return generate_one_param_function_clause('LOG', column, provider)
+
+
+def power(data_faker, column, provider):
+    return generate_two_param_function_clause('POWER', column, provider)
+
+
+def crate_random(data_faker, column, provider):
+    gen_value = random.random()
+    return f"RANDOM() < {gen_value}"
+
+
+def round(data_faker, column, provider):
+    return generate_one_param_function_clause('ROUND', column, provider)
+
+
+def sqrt(data_faker, column, provider):
+    return generate_one_param_function_clause('SQRT', column, provider, True)
+
+
+def sin(data_faker, column, provider):
+    return generate_one_param_function_clause('SIN', column, provider)
+
+
+def asin(data_faker, column, provider):
+    return generate_arc_function_clause('ASIN', column, provider)
+
+
+def cos(data_faker, column, provider):
+    return generate_one_param_function_clause('COS', column, provider)
+
+
+def acos(data_faker, column, provider):
+    return generate_arc_function_clause('ACOS', column, provider)
+
+
+def tan(data_faker, column, provider):
+    return generate_one_param_function_clause('TAN', column, provider)
+
+
+def atan(data_faker, column, provider):
+    return generate_arc_function_clause('ATAN', column, provider)
+
+# String functions
+
+
+def concat(data_faker, column, provider):
+    return generate_two_param_function_clause('CONCAT', column, provider)
+
+
+def format(data_faker, column, provider):
+    return f"FORMAT('%s', {column}) = {column}"
+
+
+def substr(data_faker, column, provider):
+    return f"SUBSTR({column},0,1) < {column}"
+
+
+def char_length(data_faker, column, provider):
+    gen_length = data_faker.fake.random_int(min=1, max=80)
+    return f"CHAR_LENGTH({column}) < {gen_length}"
+
+
+def bit_length(data_faker, column, provider):
+    gen_length = data_faker.fake.random_int(min=1, max=64)
+    return f"BIT_LENGTH({column}) < {gen_length}"
+
+
+def octet_length(data_faker, column, provider):
+    gen_length = data_faker.fake.random_int(min=1, max=8)
+    return f"OCTET_LENGTH({column}) < {gen_length}"
+
+
+def lower(data_faker, column, provider):
+    return generate_one_param_function_clause('LOWER', column, provider)
+
+
+def upper(data_faker, column, provider):
+    return generate_one_param_function_clause('UPPER', column, provider)
+
+
+def sha1(data_faker, column, provider):
+    return generate_one_param_function_clause('SHA1', column, provider)
+
+
+def md5(data_faker, column, provider):
+    return generate_one_param_function_clause('MD5', column, provider)
+
+# Date/Time functions
+
+
+def date_trunc(data_faker, column, provider):
+    return f"DATE_TRUNC('day', {column}) < 30"
+
+
+def date_format(data_faker, column, provider):
+    return generate_one_param_function_clause('DATE_FORMAT', column, provider)
+
+
 def every(x):
     return random.randint(1, x) == 1
+
+
+def type_is_number(type):
+    return type in NUMBER_TYPES
 
 
 CONJUNCTIONS = ('AND', 'OR')
@@ -43,6 +234,14 @@ OP_SYMBOLS = (
     '>=',
     '<',
     '<=',
+)
+NUMBER_TYPES = (
+    'byte',
+    'short',
+    'integer',
+    'long',
+    'float',
+    'double',
 )
 OPERATORS_BY_TYPE = {
     'byte': OP_SYMBOLS,
@@ -57,8 +256,14 @@ OPERATORS_BY_TYPE = {
     'boolean': OP_SYMBOLS,
 }
 SCALARS_BY_TYPE = {
-    'geo_shape': (within,),
-    'geo_point': (distance,),
+    'array': (array_cat, array_unique, array_difference,),
+    'geo_shape': (within, intersects,),
+    'geo_point': (distance, latitude, longitude, geohash,),
+    'number': (abs, ceil, floor, ln, log, power, crate_random, round, sqrt, sin,
+               asin, cos, acos, tan, atan,),
+    'string': (concat, format, substr, char_length, bit_length, octet_length,
+               lower, upper, sha1, md5,),
+    'timestamp': (date_trunc, date_format,),
 }
 
 CREATE_TABLE = '''
@@ -116,7 +321,10 @@ def rnd_expr(data_faker, columns):
         inner_type, *dimensions = data_type.split('_array')
     provider = data_faker.provider_for_column(column, inner_type)
     use_scalar = every(15)
-    scalars = SCALARS_BY_TYPE.get(data_type, [])
+    if type_is_number(data_type):
+        scalars = SCALARS_BY_TYPE.get('number', [])
+    else:
+        scalars = SCALARS_BY_TYPE.get(data_type, [])
     scalar = scalars and random.choice(scalars)
     if use_scalar and scalar:
         expr = scalar(data_faker, column, provider)
