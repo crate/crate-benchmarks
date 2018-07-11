mfussenegger/cr8

commit 3ef2040dd49eeeee4937f2e35d368e1e15651313

Change timestamp default faker to -2y - now
And use `datetime` instead of a unix timestamp to make it work with the
postgres client as well.

cr8/clients.py
@@ -224,7 +224,7 @@ class AsyncpgClient:
         pool = await self._get_pool()
         async with pool.acquire() as conn:
             if args:
-                rows = await conn.fetch(stmt, args)
+                rows = await conn.fetch(stmt, *args)
             else:
                 rows = await conn.fetch(stmt)
             return {

cr8/insert_fake_data.py
@@ -51,16 +51,6 @@ def generate_row(fakers):
     return [x() for x in fakers]
 
 
-def x1000(func):
-    return func() * 1000
-
-
-def timestamp(fake):
-    # return lamda: fake.unix_time() * 1000 workaround:
-    # can't use lambda or nested functions because of multiprocessing pickling
-    return partial(x1000, fake.unix_time)
-
-
 def array_provider(len_provider, value_provider, dimensions):
     if dimensions == 0:
         return value_provider()
@@ -94,7 +84,8 @@ class DataFaker:
         'float': operator.attrgetter('pyfloat'),
         'double': operator.attrgetter('pydecimal'),
         'ip': operator.attrgetter('ipv4'),
-        'timestamp': timestamp,
+        'timestamp': lambda f: partial(
+            f.date_time_between, start_date='-2y', end_date='now'),
         'string': operator.attrgetter('word'),
         'boolean': operator.attrgetter('boolean'),
         'geo_point': operator.attrgetter('geo_point'),

tests/test_insert_fake_data.py
@@ -3,6 +3,7 @@ from cr8 import insert_fake_data
 from unittest import TestCase, main
 from doctest import DocTestSuite
 from decimal import Decimal
+import datetime
 
 
 class TestDataFaker(TestCase):
@@ -43,11 +44,15 @@ class TestDataFaker(TestCase):
 
     def test_timestamp_column_default(self):
         provider = self.f.provider_for_column('timestamp', 'timestamp')
-        self.assertEqual(provider(), 1373158606000)
+        dt = provider()
+        diff = datetime.datetime(2017, 11, 18, 19, 0, 0) - dt
+        self.assertLessEqual(diff, datetime.timedelta(seconds=1))
 
     def test_timestamp_type_default(self):
         provider = self.f.provider_for_column('some_ts_column', 'timestamp')
-        self.assertEqual(provider(), 1373158606000)
+        dt = provider()
+        diff = datetime.datetime(2017, 11, 18, 19, 0, 0) - dt
+        self.assertLessEqual(diff, datetime.timedelta(seconds=1))
 
     def test_provider_from_mapping(self):
         mapping = {'x': ['random_int', [10, 20]]}

crate/crate-benchmarks

commit 4d7b7c473efb85b4dae20b683863234775b1bb85

rnd_queries: Use unix_time for timestamp value generation
The change in cr8 https://github.com/mfussenegger/cr8/commit/3ef2040dd49eeeee4937f2e35d368e1e15651313 broke the value generation for timestamps, leading to queries like this:

    SELECT count(*) FROM \"benchmarks\".\"query_tests\" WHERE 2016-08-20
    23:16:44 >= ANY (\"atimestamp\") AND \"slong\" <= -3771834702135012260
    AND \"stimestamp\" != 2017-02-19 23:43:01;

query_tests/generate_rnd_queries.py
@@ -124,6 +124,7 @@ def abs(data_faker, column, provider):
     compare_to = provider()
     return f"({column}>0 AND abs(({column}-{column}) + ({column}/{column})) != {compare_to})"
 
+
 def ceil(data_faker, column, provider):
     compare_to = data_faker.provider_for_column("byte", "byte")()
     return f"({column}!=0 AND CEIL({column}/{column}) != {compare_to})"
@@ -407,7 +408,11 @@ def rnd_expr(data_faker, columns):
         column = random.choice(list(columns.keys()))
         data_type = columns[column]
         inner_type, *dimensions = data_type.split('_array')
-    provider = data_faker.provider_for_column(column, inner_type)
+    if inner_type == 'timestamp':
+        def provider():
+            return data_faker.fake.unix_time() * 1000
+    else:
+        provider = data_faker.provider_for_column(column, inner_type)
     if inner_type in TYPE_REQUIRES_QUOTES:
         provider = partial(add_quotes, provider)
     use_scalar = every(15)