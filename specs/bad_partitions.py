f986424fdb8863055a170184a90162f103912249
Mathias Fussenegger <f.mathias@zignar.net>
2019-07-12 10:45:01 +0200

    Add a spec that creates 400 partitions with 10p(+10r) shards each
    And then keeps inserting 1e6 records

---
+++ specs/bad_partitions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""cr8 spec to create too many partitions."""
+
+from cr8.bench_spec import Spec, Instructions  # type: ignore
+from itertools import cycle
+from uuid import uuid4
+
+
+CREATE_TABLE = '''
+CREATE TABLE tp (
+    id STRING PRIMARY KEY,
+    p INTEGER PRIMARY KEY
+) CLUSTERED INTO 10 SHARDS PARTITIONED BY (p)
+'''
+
+
+def _queries():
+    num = 400
+    c = cycle(list(range(num)))
+    yield {
+        'statement': 'insert into tp (id, p) values (?, ?)',
+        'args': lambda: [str(uuid4()), next(c)],
+        'concurrency': 25,
+        'iterations': int(1e6)
+    }
+
+
+spec = Spec(
+    setup=Instructions(statements=[CREATE_TABLE]),
+    teardown=Instructions(statements=["DROP TABLE tp"]),
+    queries=_queries(),
+)
+
+
+if __name__ == "__main__":
+    for q in _queries():
+        for _ in range(5):
+            print(q['args']())
