a34eceb75b2488333d2dc7baba0f922edb23148f
Mathias Fussenegger <f.mathias@zignar.net>
2020-03-24 15:27:16 +0100

    Fetch amplab uservisits data from our cdn

--- specs/sql/uservisits_large_and_small.sql
+++ specs/sql/uservisits_large_and_small.sql
@@ -26,10 +26,20 @@ CREATE TABLE uservisits_small (
       number_of_replicas = 0,
       refresh_interval = 0
     );
-copy uservisits_small from 's3://crate.amplab/data/1node/uservisits/part-00000*' with (compression = 'gzip');
-copy uservisits_small from 's3://crate.amplab/data/1node/uservisits/part-00001*' with (compression = 'gzip');
-copy uservisits_small from 's3://crate.amplab/data/1node/uservisits/part-00002*' with (compression = 'gzip');
-copy uservisits_small from 's3://crate.amplab/data/1node/uservisits/part-00003*' with (compression = 'gzip');
-copy uservisits_small from 's3://crate.amplab/data/1node/uservisits/part-00004*' with (compression = 'gzip');
-copy uservisits_large from 's3://crate.amplab/data/1node/uservisits/part-0000*' with (compression = 'gzip');
+copy uservisits_small from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00000.gz' with (compression = 'gzip');
+copy uservisits_small from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00001.gz' with (compression = 'gzip');
+copy uservisits_small from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00002.gz' with (compression = 'gzip');
+copy uservisits_small from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00003.gz' with (compression = 'gzip');
+copy uservisits_small from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00004.gz' with (compression = 'gzip');
+
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00000.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00001.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00002.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00003.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00004.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00005.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00006.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00007.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00008.gz' with (compression = 'gzip');
+copy uservisits_large from 'https://cdn.crate.io/downloads/datasets/amplab/1node/uservisits/part-00009.gz' with (compression = 'gzip');
 refresh table uservisits_large, uservisits_small;
