6fddd26e64d9094bd558a501a1beea70e63bfd8f
Mathias Fussenegger <f.mathias@zignar.net>
2020-05-18 17:52:25 +0200

    Tune compare_measures output
    Example:
    
        C: 1
        | Version |         Mean ±    Stdev |        Min |     Median |         Q3 |        Max |
        |   V1    |      519.090 ±   31.357 |    472.204 |    513.366 |    547.925 |    624.908 |
        |   V2    |      258.759 ±   23.758 |    198.775 |    266.419 |    278.941 |    286.146 |
        ├---------┴-------------------------┴------------┴------------┴------------┴------------┘
        |               -  66.94%                           -  63.34%
        | V1 mean is within 457.63 - 580.55
        | V2 mean is within 212.19 - 305.32
        There is a 100.00% probability that the observed difference is not random or due to noise, and the best estimate of that difference is 66.94%

--- compare_measures.py
+++ compare_measures.py
@@ -9,6 +9,7 @@ The measures files should contain 1 number per line
 """
 
 import argparse
+import numpy as np
 from scipy import stats
 from cr8 import metrics
 
@@ -26,30 +27,35 @@ class Diff:
     def __init__(self, r1, r2):
         self.r1 = r1
         self.r2 = r2
-        r1_samples = r1.get('samples', [r1['mean']])
-        r2_samples = r2.get('samples', [r2['mean']])
+        r1_samples = np.array(r1.get('samples', [r1['mean']]))
+        r2_samples = np.array(r2.get('samples', [r2['mean']]))
         ind = stats.ttest_ind(r1_samples, r2_samples)
-        tscore = ind.statistic
+        self.r1_ci = stats.norm.interval(0.95, loc=r1_samples.mean(), scale=r1_samples.std(ddof=1))
+        self.r2_ci = stats.norm.interval(0.95, loc=r2_samples.mean(), scale=r2_samples.std(ddof=1))
         self.mean_diff = perc_diff(r1['mean'], r2['mean'])
         self.median_diff = perc_diff(r1['percentile']['50'], r2['percentile']['50'])
-        if abs(tscore) >= CRITICAL_VALUE:
-            self.significance = 'Likely significant'
+        probability = (1 - ind.pvalue) * 100.0
+        self.ptext = f'There is a {probability:.2f}% probability that the observed difference is not random or due to noise, and the best estimate of that difference is {self.mean_diff:.2f}%'
+        if abs(ind.statistic) >= CRITICAL_VALUE:
+            self.significance = 'The test has statistical significance'
         else:
-            self.significance = 'Likely NOT significant'
+            self.significance = 'The test has no statistical significance'
 
     def __str__(self):
         return json.dumps(self.__dict__)
 
 
 def print_diff(diff):
+    mean_prefix = '+' if diff.r1['mean'] < diff.r2['mean'] else '-'
+    median_prefix = '+' if diff.r1['percentile']['50'] < diff.r2['percentile']['50'] else '-'
     print(f'| Version |         Mean ±    Stdev |        Min |     Median |         Q3 |        Max |')
     print(f"|   V1    |   {diff.r1['mean']:10.3f} ± {diff.r1['stdev']:8.3f} | {diff.r1['min']:10.3f} | {diff.r1['percentile']['50']:10.3f} | {diff.r1['percentile']['75']:10.3f} | {diff.r1['max']:10.3f} |")
     print(f"|   V2    |   {diff.r2['mean']:10.3f} ± {diff.r2['stdev']:8.3f} | {diff.r2['min']:10.3f} | {diff.r2['percentile']['50']:10.3f} | {diff.r2['percentile']['75']:10.3f} | {diff.r2['max']:10.3f} |")
-
-    mean_prefix = '+' if diff.r1['mean'] < diff.r2['mean'] else '-'
-    median_prefix = '+' if diff.r1['percentile']['50'] < diff.r2['percentile']['50'] else '-'
-    print(f'mean:   {mean_prefix}{diff.mean_diff:7.2f}%')
-    print(f'median: {median_prefix}{diff.median_diff:7.2f}%')
+    print(f'├---------┴-------------------------┴------------┴------------┴------------┴------------┘')
+    print(f"|               {mean_prefix}{diff.mean_diff:7.2f}%                           {median_prefix}{diff.median_diff:7.2f}%   ")
+    print(f'| V1 mean is within {diff.r1_ci[0]:.2f} - {diff.r1_ci[1]:.2f}')
+    print(f'| V2 mean is within {diff.r2_ci[0]:.2f} - {diff.r2_ci[1]:.2f}')
+    print(f'{diff.ptext}')
     print(f'{diff.significance}')
     print('')
 
