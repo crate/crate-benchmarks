8bfe42a95ecb25a4fe9ff0b3c9f5fbf6467732d7
Eylon Ronen <eylon.ronen@gmail.com>
2025-03-18 12:41:36 +0200

    Add bringg benchmark with setup and teardown

---
+++ specs/sql/bringg_teardown.sql
@@ -0,0 +1,7 @@
+DROP TABLE customers;
+DROP TABLE way_points;
+DROP TABLE tasks;
+DROP ANALYZER ngram_lowercase_analyzer;
+DROP ANALYZER edge_ngram_lowercase_analyzer;
+DROP ANALYZER edge_ngram_full_field_analyzer;
+DROP ANALYZER edge_ngram_phone_analyzer;
